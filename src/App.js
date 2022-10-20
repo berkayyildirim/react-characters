5e11e56da0a9b5fe16fd74737e15dbc92265c9ae
berkayyildirim <huseyinberkayyildirim@gmail.com>
2022-10-20 16:56:46 +0300

    implement useEffect and axios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [charactersArr, setCharactersArr] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://ih-crud-api.herokuapp.com/characters")
+      .then((response) => {
+        setCharactersArr(response.data);
+      })
+      .catch((e) => console.log("error getting characters from API", e));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h2>List of characters in our API:</h2>
+
+      {charactersArr.map((character) => {
+        return (
+          <div className='box'>
+            <h3>{character.name}</h3>
+            <p>Occupation {character.occupation}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
